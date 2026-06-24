8c3c74e593deef76cf124497fc57996aac5eecee
Snorro1998 <tjaard.van.verseveld@gmail.com>
2026-06-24 02:18:01 +0200

    Fixed a bug that made town npcs invulnerable to most enemies

--- Content/NPCs/ChangedNPC.cs
+++ Content/NPCs/ChangedNPC.cs
@@ -1170,7 +1170,10 @@ namespace ChangedSpecialMod.Content.NPCs
         {
             var changedNPC = npc.Changed();
             var attackerChangedNPC = attacker.Changed();
-            if (changedNPC != null && attackerChangedNPC != null && changedNPC.GooType == attackerChangedNPC.GooType)
+            if (changedNPC != null && attackerChangedNPC != null)
+                return true;
+
+            if (changedNPC.GooType != GooType.Invalid && changedNPC.GooType == attackerChangedNPC.GooType)
                 return false;
             return true;
         }
